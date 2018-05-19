97b1b1358d7bf71f632d56d3ea3e34a1bc475395
Johannes Hilden <hildenjohannes@gmail.com>
2018-05-19 21:27:07 +0300

    Put stuff in the right place

--- Ginac/FFI.hs
+++ Ginac/FFI.hs
@@ -5,15 +5,3 @@ import Foreign
 data GinacExpr
 
 type Ginac = ForeignPtr GinacExpr
-
-instance Num Expr where
-    x + y        = undefined
-    x * y        = undefined
-    negate       = undefined
-    abs    x     = undefined
-    signum x     = undefined
-    fromInteger  = undefined
-
-instance Fractional Expr where
-    x / y        = undefined
-    fromRational = undefined
--- Powerpants.hs
+++ Powerpants.hs
@@ -5,3 +5,15 @@ import Ginac.FFI
 data Expr = Expr Ginac | X
 
 data GF = GF Expr
+
+instance Num Expr where
+    x + y        = undefined
+    x * y        = undefined
+    negate       = undefined
+    abs    x     = undefined
+    signum x     = undefined
+    fromInteger  = undefined
+
+instance Fractional Expr where
+    x / y        = undefined
+    fromRational = undefined
