3a6d0f07ce2fad5e7087f0d9bc772437e0f91cad
Johannes Hilden <hildenjohannes@gmail.com>
2018-05-20 14:17:33 +0300

    Update example in main

--- Main.hs
+++ Main.hs
@@ -4,8 +4,8 @@ import Powerpants
 
 main :: IO ()
 main = do
-    let ax = GF $ 1 / (1 - x)
-    let bx = GF $ 1 / (1 - x)
+    let ax = GF (1 + x) -- GF $ 1 / (1 - x)
+    let bx = GF (2 + x) -- $ 1 / (1 - x)
     let cx = ax + bx
     printGF cx
     pure ()
