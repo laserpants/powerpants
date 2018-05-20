f2f5b00ce09b00582834572ae25e82f746949f65
Johannes Hilden <hildenjohannes@gmail.com>
2018-05-20 13:08:29 +0300

    Move Expr data type to Ginac module

--- Powerpants.hs
+++ Powerpants.hs
@@ -1,22 +1,8 @@
 module Powerpants where
 
-import Ginac.FFI
-
-newtype Expr = Expr Ginac
+import Ginac
 
 newtype GF = GF Expr
-
-instance Num Expr where
-    x + y        = undefined
-    x * y        = undefined
-    negate       = undefined
-    abs    x     = undefined
-    signum x     = undefined
-    fromInteger  = undefined
-
-instance Fractional Expr where
-    x / y        = undefined
-    fromRational = undefined
 
 --instance Num GF where
 --    x + y        = undefined
