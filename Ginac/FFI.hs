7dc424f735be37e6e7777f135dce4f0e7e455c40
Johannes Hilden <hildenjohannes@gmail.com>
2018-05-20 13:07:43 +0300

    Build out FFI functions

--- Ginac/FFI.hs
+++ Ginac/FFI.hs
@@ -8,6 +8,16 @@ data GinacSymbol
 type Ginac = ForeignPtr GinacEx
 
 foreign import ccall "ginac_symbol"
-    c_ginac_symbol :: IO (Ptr GinacSymbol)
+    ginac_symbol :: IO (Ptr GinacSymbol)
 
+foreign import ccall "ginac_ex_new_symbol"
+    ginac_ex_new_symbol :: Ptr GinacSymbol -> IO (Ptr GinacEx)
 
+foreign import ccall "ginac_ex_new_x"
+    ginac_ex_new_x :: IO (Ptr GinacEx)
+
+foreign import ccall "ginac_ex_free"
+    ginac_ex_free :: Ptr GinacEx -> IO ()
+
+foreign import ccall "&ginac_ex_free"
+    ginac_ex_free_fun :: FunPtr (Ptr GinacEx -> IO ())
