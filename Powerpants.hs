f60a67b076b729450def52405c781e6cec6e1c30
Johannes Hilden <hildenjohannes@gmail.com>
2018-05-21 15:12:42 +0300

    Implement eval

--- Powerpants.hs
+++ Powerpants.hs
@@ -2,10 +2,11 @@
 module Powerpants
   ( OGF(..)
   , Powerpants.Ginac.diff
-  , Powerpants.Ginac.eval
+  , Powerpants.Ginac.subs
   , Powerpants.Ginac.factorial
   , Powerpants.Ginac.pow
   , Powerpants.Ginac.sqrt
+  , Powerpants.eval
   , x
   , printGF
   ) where
@@ -16,6 +17,9 @@ newtype OGF = OGF Expr deriving (Num, Fractional)
 
 x :: OGF
 x = OGF (Ex symbol)
+
+eval :: OGF -> Int -> Maybe Double
+eval (OGF ex) = Powerpants.Ginac.eval ex
 
 printGF :: OGF -> IO ()
 printGF (OGF ex) = printEx ex
