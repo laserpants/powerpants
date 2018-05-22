d35c0740a423fed0a7304edbbe1bae786142a012
Johannes Hilden <hildenjohannes@gmail.com>
2018-05-22 09:54:41 +0300

    Add INLINE pragmas

--- Powerpants.hs
+++ Powerpants.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Powerpants
---  ( OGF(..)
   ( OGF(..)
   , Powerpants.diff
   , Powerpants.eval
@@ -15,13 +14,17 @@ import qualified Powerpants.Ginac as Ex
 newtype OGF = Gx Expr deriving (Num, Fractional)
 
 x :: OGF
+{-# INLINE x #-}
 x = Gx (Ex Ex.symbol)
 
 eval :: OGF -> Int -> Maybe Double
+{-# INLINE eval #-}
 eval (Gx ex) = Ex.eval ex
 
 diff :: OGF -> OGF
+{-# INLINE diff #-}
 diff (Gx ex) = Gx (Ex.diff ex)
 
 toString :: OGF -> String
+{-# INLINE toString #-}
 toString (Gx ex) = Ex.toString ex
