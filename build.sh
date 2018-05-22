5dfb8393dc436628261e83073b207ba0a675f600
Johannes Hilden <hildenjohannes@gmail.com>
2018-05-22 09:58:02 +0300

    Add -O3 flag to compile command

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++ -c -o pp_ginac.o pp_ginac.cpp -lginac -lcln --std=c++11 -Wall \
+g++ -c -o pp_ginac.o pp_ginac.cpp -lginac -lcln --std=c++11 -Wall -O3 \
   && ghc --make Main.hs -o main pp_ginac.o -lginac -lcln -lstdc++
