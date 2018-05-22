1c788be48337a94659338743a9a850f93489b903
Johannes Hilden <hildenjohannes@gmail.com>
2018-05-22 09:59:00 +0300

    Build out examples more

--- Main.hs
+++ Main.hs
@@ -1,30 +1,52 @@
 module Main where
 
-import Data.Maybe ( fromJust )
+import Data.Maybe ( fromJust, fromMaybe )
 import Powerpants
 import Powerpants.Ginac
 
-take :: Int -> OGF -> [Integer]
-take m gf = (round . fromJust) <$> rec 0 gf where
-  rec n (OGF ex) = if m == n
-    then []
-    else Powerpants.eval (OGF (ex/factorial n)) 0 : rec (n + 1) (OGF (diff ex))
+import qualified Powerpants as GF
+
+list :: OGF -> [Integer]
+list gf = maybe 0 round <$> rec 0 gf where
+  rec n ex = GF.eval (ex / Gx (factorial n)) 0 : rec (n + 1) (GF.diff ex)
+
+--take :: Int -> OGF -> [Integer]
+--take m gf = (round . fromJust) <$> rec 0 gf where
+--  rec n ex = 
+--    let f = Gx (factorial n)
+--     in if m == n 
+--          then []
+--          else GF.eval (ex / f) 0 : rec (n + 1) (GF.diff ex)
+
+--toList :: OGF -> [Integer]
+--toList gf = [cx gf i | i <- [0..100]]
 
 main :: IO ()
 main = do
-    let ax = x / (1 - x - x^2) :: OGF
+    let ax = x/(1-x-x^2)
 
-    -- print (Main.take 614 ax)
+    print (GF.toString ax)
 
-    let bx = 1 / (1 - x) :: OGF
-    let cx = 2 / (1 - x) :: OGF
-    let dx = bx + cx
+    print (list ax !! 600)
 
-    print (Main.take 10 ax)
+    --let str = toString ax 
 
-    print (Main.take 10 bx)
-    print (Main.take 10 cx)
+    --putStrLn str
 
-    print (Main.take 10 dx)
+--    -- print (Main.take 614 ax)
+--
+--    let bx = Gx $ 1 / (1 - x)
+--    let cx = Gx $ 2 / (1 - x)
+--    let dx = bx + cx
+
+    --putStrLn (GF.toString ax)
+
+--    print (toList ax !! )
+--    print (Main.take 533 ax)
+
+--    print (Main.take 133 bx)
+--    print (Main.take 133 cx)
+--
+--    print (Main.take 133 dx)
 
     pure ()
