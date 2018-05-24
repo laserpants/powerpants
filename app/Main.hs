d68c4c0026335e71402ac264435331d66b8ada45
Johannes Hilden <hildenjohannes@gmail.com>
2018-05-24 16:15:42 +0300

    Update git commit hash

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,11 @@ module Main where
 import Powerpants
 
 main :: IO ()
-main = pure ()
+main = do
+    
+    -- let gx = 1/(1 - x) :: OGF
+    let f = var x + 5 :: OGF
+
+    print (substitute f 5)
+
+    pure ()
--- src/Powerpants.hs
+++ src/Powerpants.hs
@@ -3,6 +3,33 @@ module Powerpants where
 
 import Math.Ginac
 import Math.Ginac.Symbolic
+import System.IO.Unsafe
 
 newtype OGF = Gx Expr deriving (Num, Fractional)
 
+factorial :: Int -> OGF
+factorial n = undefined
+
+derivative :: OGF -> OGF
+derivative (Gx ex) = undefined
+
+substitute :: OGF -> Int -> Maybe Double
+substitute = undefined
+
+coefficient :: OGF -> Int -> Integer
+coefficient gf nth = expand gf !! nth
+
+expand :: OGF -> [Integer]
+expand = fmap cast . rec 0 where
+    cast :: Maybe Double -> Integer
+    cast = maybe 0 round
+    rec :: Int -> OGF -> [Maybe Double]
+    rec n gf = x:xs where
+        x    = substitute (gf / Powerpants.factorial n) 0
+        xs   = rec (succ n) (derivative gf)
+
+xsym :: Symbol
+xsym = undefined
+
+x :: OGF
+x = undefined
