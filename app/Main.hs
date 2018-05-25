b61f05620f167a53fdc78a9dd13f44e42ad3506e
Johannes Hilden <hildenjohannes@gmail.com>
2018-05-25 10:21:25 +0300

    Implement expanded etc.

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,27 @@ import Powerpants
 
 main :: IO ()
 main = do
-    
-    -- let gx = 1/(1 - x) :: OGF
-    let f = var x + 5 :: OGF
 
-    print (substitute f 5)
+    let gx = X/(1 - X - X^2)
+    print (expandedTo 20 gx)
+
+    let fx = lsh 2 gx
+    print (expandedTo 20 fx)
+
+    -- let fx = rsh 2 gx
+    -- print (expandedTo 20 fx)
+
+    -- let ex = lsh 2 fx
+    -- print (expandedTo 20 ex)
+    -- print (expandedTo 20 ex == expandedTo 20 gx)
+
+--    let ax = 1/(1 - X)
+--    print (expandedTo 20 ax)
+--
+--    let bx = ddx ax
+--    print (expandedTo 20 bx)
+--
+--    let cx = ax + bx
+--    print (expandedTo 20 cx)
 
     pure ()
--- src/Powerpants.hs
+++ src/Powerpants.hs
@@ -1,36 +1,104 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-module Powerpants where
+module Powerpants
+  ( OGF(..)
+  , ddx
+  , expanded
+  , expandedTo
+  , lsh
+  , rsh
+  ) where
 
-import Math.Ginac
+import Control.Monad
+import Data.Ratio
+import Foreign
 import Math.Ginac.FFI
-import Math.Ginac.Symbolic
+import Math.Ginac.FFI.ForeignPtr
 import System.IO.Unsafe
 
-newtype OGF = Gx Expr deriving (Num, Fractional)
+data OGF = Gx (ForeignPtr GinacEx) | X
+
+instance Num OGF where
+    (+)          = Powerpants.add
+    (*)          = Powerpants.mul
+    negate       = Powerpants.neg
+    abs          = Powerpants.abs
+    signum       = Powerpants.sig
+    fromInteger  = Powerpants.num
+
+instance Fractional OGF where
+    (/)          = Powerpants.div
+    fromRational = Powerpants.rat
+
+gx :: IO (Ptr GinacEx) -> OGF
+gx ioptr = Gx (unsafePerformIO (ioptr >>= newForeignPtr ginac_ex_free_fun))
+
+num :: Integral a => a -> OGF
+num = gx . ginac_ex_new_from_int . fromIntegral
+
+withOGF :: OGF -> (Ptr GinacEx -> IO b) -> IO b
+withOGF (Gx ptr) fn = withForeignPtr ptr fn
+withOGF        X fn = ginac_symbol_static >>= ginac_ex_new_from_basic >>= fn
+
+binop :: (Ptr GinacEx -> Ptr GinacEx -> IO c) -> OGF -> OGF -> IO c
+binop op f g = withOGF f (withOGF g . op)
+
+add :: OGF -> OGF -> OGF
+add ax bx = gx (binop ginac_add ax bx)
+
+mul :: OGF -> OGF -> OGF
+mul ax bx = gx (binop ginac_mul ax bx)
+
+neg :: OGF -> OGF
+neg ax = gx (withOGF ax ginac_ex_neg)
+
+abs :: OGF -> OGF
+abs ax = gx (withOGF ax ginac_ex_abs)
+
+sig :: OGF -> OGF
+sig ax = gx (withOGF ax ginac_ex_signum)
+
+div :: OGF -> OGF -> OGF
+div ax bx = gx (binop ginac_div ax bx)
+
+pow :: OGF -> OGF -> OGF
+pow ax bx = gx (binop ginac_pow ax bx)
+
+rat :: Rational -> OGF
+rat r = Powerpants.div (num n) (num d) where
+    n = fromInteger (numerator r)
+    d = fromInteger (denominator r)
+
+ddx :: OGF -> OGF
+ddx ax = gx (withOGF ax fn) where
+    fn ptr = ginac_symbol_static
+         >>= ginac_diff 1 ptr
 
 factorial :: Int -> OGF
-factorial n = undefined
+factorial n = gx (ginac_factorial n)
 
-derivative :: OGF -> OGF
-derivative (Gx ex) = undefined
+numcast :: Ptr GinacEx -> IO (Maybe Double)
+numcast ptr = do
+    isNumeric <- ginac_ex_is_numeric ptr
+    if isNumeric
+        then fmap Just (ginac_ex_to_double ptr)
+        else pure Nothing
 
 substitute :: OGF -> Int -> Maybe Double
-substitute = undefined
+substitute ax x = unsafePerformIO (withOGF ax fn) where
+    fn ptr = ginac_symbol_static >>= ginac_ex_subs_int x ptr >>= numcast
 
-coefficient :: OGF -> Int -> Integer
-coefficient gf nth = expand gf !! nth
+expanded :: OGF -> [Integer]
+expanded ax = maybe 0 round <$> series ax where
+    series :: OGF -> [Maybe Double]
+    series = rec 0 where
+        rec n ax = x:xs where
+            x  = substitute (ax / factorial n) 0
+            xs = rec (succ n) (ddx ax)
 
-expand :: OGF -> [Integer]
-expand = fmap cast . rec 0 where
-    cast :: Maybe Double -> Integer
-    cast = maybe 0 round
-    rec :: Int -> OGF -> [Maybe Double]
-    rec n gf = x:xs where
-        x    = substitute (gf / Powerpants.factorial n) 0
-        xs   = rec (succ n) (derivative gf)
+expandedTo :: Int -> OGF -> [Integer]
+expandedTo n = take n . expanded
 
-xsym :: Symbol
-xsym = undefined
+lsh :: Int -> OGF -> OGF
+lsh n ax = X^^negate n * ax
 
-x :: OGF
-x = undefined
+rsh :: Int -> OGF -> OGF
+rsh n ax = X^^n * ax
