27dc12350348a1bc6be4227c6e6c087360fdb55a
Johannes Hilden <hildenjohannes@gmail.com>
2018-05-26 18:39:03 +0300

    Commit experimental stuff

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,73 @@
 module Main where
 
+import System.IO.Unsafe
 import Powerpants
+import Math.Ginac.FFI
+import Foreign
 
 main :: IO ()
 main = do
 
     let gx = X/(1 - X - X^2)
-    print (expandedTo 20 gx)
+----    print (expand 20 gx)
+--
+--    print (take 800 (expnd2 gx))
 
-    let fx = lsh 2 gx
-    print (expandedTo 20 fx)
 
-    -- let fx = rsh 2 gx
-    -- print (expandedTo 20 fx)
+--
 
-    -- let ex = lsh 2 fx
-    -- print (expandedTo 20 ex)
-    -- print (expandedTo 20 ex == expandedTo 20 gx)
+    print (take 902 (expnd2 gx))
 
+--    x <- ginac_symbol_static
+--    p <- ginac_ex_new_from_basic x
+--    z <- ginac_ex_new_from_int 0
+--    r <- ginac_relation_eq_new p z
+--    s <- withExPtr gx (\ptr -> ginac_ex_series ptr r 900)
+--    ss <- newForeignPtr ginac_ex_free_fun s
+--
+--    print (take 100 (expnd3 s x))
+
+    --ginac_ex_free s
+
+
+--
+
+
+
+--    print (expnd gx 30)
+--    print (coefficient gx 555)
+
+    --print (take 30 $ fmap unsafePerformIO (yyy gx))
+
+--    print (expnd gx )
+
+--    let hx = X^3 * gx
+--    print (expand 20 hx)
+--
+--    let fx = 2*X^2
+--    print (expand 20 fx)
+--
+--    let fx' = 1/(X^2)
+--    print (expand 20 fx')
+--
+--    print (nth 20 fx')
+--    print (nth 3 gx)
+
+--    let ix = X^^(-3) * hx
+--    print (expand 20 ix)
+--    print (expand 20 ix == expand 20 gx)
+
+--    let jx = X^^(-6) * gx
+--    print (expand 1 jx)
+    --
+--
 --    let ax = 1/(1 - X)
---    print (expandedTo 20 ax)
+--    print (expand 20 ax)
 --
 --    let bx = ddx ax
---    print (expandedTo 20 bx)
+--    print (expand 20 bx)
 --
 --    let cx = ax + bx
---    print (expandedTo 20 cx)
+--    print (expand 20 cx)
 
     pure ()
--- src/Powerpants.hs
+++ src/Powerpants.hs
@@ -1,17 +1,16 @@
-module Powerpants
-  ( OGF(..)
-  , ddx
-  , expanded
-  , expandedTo
-  , lsh
-  , rsh
-  ) where
+module Powerpants where
+--  ( OGF(..)
+--  , coefficient
+--  , ddx
+--  , expand
+--  , nth
+--  ) where
 
 import Control.Monad
 import Data.Ratio
 import Foreign
 import Math.Ginac.FFI
-import Math.Ginac.FFI.ForeignPtr
+--import Math.Ginac.FFI.ForeignPtr
 import System.IO.Unsafe
 
 data OGF = Gx (ForeignPtr GinacEx) | X
@@ -28,52 +27,231 @@ instance Fractional OGF where
     (/)          = Powerpants.div
     fromRational = Powerpants.rat
 
+instance Eq OGF where
+    (==)         = Powerpants.eql
+
 gx :: IO (Ptr GinacEx) -> OGF
 gx ioptr = Gx (unsafePerformIO (ioptr >>= newForeignPtr ginac_ex_free_fun))
 
 num :: Integral a => a -> OGF
 num = gx . ginac_ex_new_from_int . fromIntegral
 
-withOGF :: OGF -> (Ptr GinacEx -> IO b) -> IO b
-withOGF (Gx ptr) fn = withForeignPtr ptr fn
-withOGF        X fn = ginac_symbol_static >>= ginac_ex_new_from_basic >>= fn
+withExPtr :: OGF -> (Ptr GinacEx -> IO b) -> IO b
+withExPtr (Gx ptr) fn = withForeignPtr ptr fn
+withExPtr X fn = ginac_symbol_static >>= ginac_ex_new_from_basic >>= fn
 
 binop :: (Ptr GinacEx -> Ptr GinacEx -> IO c) -> OGF -> OGF -> IO c
-binop op f g = withOGF f (withOGF g . op)
+binop op f g = withExPtr f (withExPtr g . op)
 
 add :: OGF -> OGF -> OGF
-add ax bx = gx (binop ginac_add ax bx)
+add ax bx = gx (binop ginac_ex_add ax bx)
 
 mul :: OGF -> OGF -> OGF
-mul ax bx = gx (binop ginac_mul ax bx)
+mul ax bx = gx (binop ginac_ex_mul ax bx)
 
 neg :: OGF -> OGF
-neg ax = gx (withOGF ax ginac_ex_neg)
+neg ax = gx (withExPtr ax ginac_ex_neg)
 
 abs :: OGF -> OGF
-abs ax = gx (withOGF ax ginac_ex_abs)
+abs ax = gx (withExPtr ax ginac_ex_abs)
 
 sig :: OGF -> OGF
-sig ax = gx (withOGF ax ginac_ex_signum)
+sig ax = gx (withExPtr ax ginac_ex_signum)
 
 div :: OGF -> OGF -> OGF
-div ax bx = gx (binop ginac_div ax bx)
+div ax bx = gx (binop ginac_ex_div ax bx)
 
 pow :: OGF -> OGF -> OGF
-pow ax bx = gx (binop ginac_pow ax bx)
+pow ax bx = gx (binop ginac_ex_pow ax bx)
 
 rat :: Rational -> OGF
 rat r = Powerpants.div (num n) (num d) where
     n = fromInteger (numerator r)
     d = fromInteger (denominator r)
 
+eql :: OGF -> OGF -> Bool
+eql ax bx = unsafePerformIO (withExPtr ax (withExPtr bx . ginac_ex_equal))
+
 ddx :: OGF -> OGF
-ddx ax = gx (withOGF ax fn) where
+ddx ax = gx (withExPtr ax fn) where
     fn ptr = ginac_symbol_static
-         >>= ginac_diff 1 ptr
+         >>= ginac_ex_diff 1 ptr
 
 factorial :: Int -> OGF
-factorial n = gx (ginac_factorial n)
+factorial n = gx (ginac_ex_factorial n)
+
+
+
+yyy :: OGF -> [IO Double]
+yyy ax = unsafePerformIO (withExPtr ax f) where
+  f ptr = do
+    x <- ginac_symbol_static >>= ginac_ex_new_from_basic
+    nil <- ginac_ex_new_from_int 0
+    rel <- ginac_relation_eq_new x nil
+    series <- ginac_ex_series ptr rel 4000
+    pure [ginac_ex_coeff series x i >>= ginac_ex_to_double | i <- [0..]]
+
+--unsafePerformIO (withExPtr ax fun)
+--  where
+--    fun :: Ptr GinacEx -> IO [IO Int]
+--    fun ptr = let prod = g ptr in pure [prod i | i <- [0..100]]
+--    g :: Ptr GinacEx -> Int -> IO Int
+--    g ptr n = do
+--        x <- ginac_symbol_static >>= ginac_ex_new_from_basic
+--        nil <- ginac_ex_new_from_int 0
+--        rel <- ginac_relation_eq_new x nil
+--        series <- ginac_ex_series ptr rel 400
+--        c <- ginac_ex_coeff series x n
+--        ginac_ex_to_int c
+
+
+
+
+--------------------------------------------
+--------------------------------------------
+--------------------------------------------
+--------------------------------------------
+--------------------------------------------
+--------------------------------------------
+--------------------------------------------
+
+
+--zzzzz :: OGF -> [Maybe Double]
+--zzzzz ax = undefined where -- unsafePerformIO (withExPtr ax baz) where
+--    baz :: Ptr GinacEx -> IO [IO (Maybe Double)]
+--    baz ptr = do
+--        x <- ginac_symbol_static
+--        p <- ginac_ex_new_from_basic x
+--        z <- ginac_ex_new_from_int 0
+--        r <- ginac_relation_eq_new p z
+--        s <- ginac_ex_series ptr r 400
+--        ginac_basic_free r
+--        --pure [ginac_ex_coeff_symbol j s x >>= numcast | j <- [0 .. n]]
+
+
+baz :: Int -> Ptr GinacEx -> Ptr GinacSymbol -> [IO (Ptr GinacEx)]
+baz n s x = ginac_ex_coeff_symbol n s x : baz (n + 1) s x
+
+fez :: OGF -> [IO (Ptr GinacEx)]
+fez ax = unsafePerformIO (withExPtr ax fn) where
+    fn ptr = do 
+        x <- ginac_symbol_static
+        p <- ginac_ex_new_from_basic x
+        z <- ginac_ex_new_from_int 0
+        r <- ginac_relation_eq_new p z
+        s <- ginac_ex_series ptr r 400
+        pure (baz 0 s x)
+
+--asdfasfd :: OGF -> [Ptr GinacEx]
+asdfasfd ax = fmap (unsafePerformIO . numcast . unsafePerformIO) (fez ax)
+
+slafs ax = sequence (fez ax)
+
+coefficient :: OGF -> Int -> Maybe Double
+coefficient ax n = unsafePerformIO (withExPtr ax fn) where
+    fn :: Ptr GinacEx -> IO (Maybe Double)
+    fn ptr = do 
+        x <- ginac_symbol_static
+        p <- ginac_ex_new_from_basic x
+        z <- ginac_ex_new_from_int 0
+        r <- ginac_relation_eq_new p z
+        s <- ginac_ex_series ptr r 600
+        ginac_basic_free r
+        a <- ginac_ex_coeff_symbol n s x
+        ginac_ex_free s
+        ginac_ex_free z
+        ginac_ex_free p
+        numcast a
+
+expnd4 :: OGF -> [Maybe Double]
+expnd4 ax = unsafePerformIO (withExPtr ax f)
+  where
+    f ptr = do
+        x <- ginac_symbol_static
+        p <- ginac_ex_new_from_basic x
+        z <- ginac_ex_new_from_int 0
+        r <- ginac_relation_eq_new p z
+        s <- ginac_ex_series ptr r 900
+--        ss <- newForeignPtr ginac_ex_free_fun s
+
+--        ginac_basic_free r
+--        ginac_ex_free z
+--        ginac_ex_free p
+
+        pure (expnd3 ptr x)
+
+expnd3' :: ForeignPtr GinacEx -> Ptr GinacSymbol -> [Double]
+expnd3' ptr x = rec 0 where
+    rec n = unsafePerformIO (withForeignPtr ptr 
+      (\expr -> ginac_ex_coeff_symbol n expr x >>= ginac_ex_to_double)) : rec (n + 1) 
+
+expnd3 :: Ptr GinacEx -> Ptr GinacSymbol -> [Maybe Double]
+expnd3 expr x = rec 0 where
+    rec n = unsafePerformIO (ginac_ex_coeff_symbol n expr x >>= numcast) : rec (n + 1) 
+
+expnd2 :: OGF -> [Double]
+expnd2 ax = expnd3' s x where
+    (s, x) = unsafePerformIO (withExPtr ax baz)
+    baz ptr = do
+        x <- ginac_symbol_static
+
+        p <- ginac_ex_new_from_basic x
+        --pp <- newForeignPtr ginac_ex_free_fun p
+
+        z <- ginac_ex_new_from_int 0
+        --zz <- newForeignPtr ginac_ex_free_fun z
+
+        r <- ginac_relation_eq_new p z
+        --rr <- newForeignPtr ginac_basic_free_fun r
+
+        --s <- ginac_ex_series ptr r 900 >>= newForeignPtr ginac_ex_free_fun 
+        s <- ginac_ex_series ptr r 900 >>= newForeignPtr ginac_ex_free_fun
+        --ss <- newForeignPtr ginac_ex_free_fun s
+
+        ginac_basic_free r
+        ginac_ex_free z
+        ginac_ex_free p
+
+        --withForeignPtr s $ \ptr -> pure (f ptr x 0)
+
+        pure (s, x) -- (f s x 0)
+        -- How to free these pointers?
+        --ginac_ex_free s
+        -- ginac_basic_free r
+        -- ginac_ex_free z
+        -- ginac_ex_free p
+
+    --ff :: Ptr GinacEx -> Ptr GinacSymbol -> Int -> [IO (Maybe Double)]
+    --ff s x n = (ginac_ex_coeff_symbol n s x >>= numcast) : ff s x (n + 1) 
+
+fun :: Ptr GinacEx -> Ptr GinacSymbol -> Int -> [Maybe Double]
+fun s x n = unsafePerformIO (ginac_ex_coeff_symbol n s x >>= numcast) : fun s x (n + 1) 
+
+expnd :: OGF -> Int -> [Maybe Double]
+expnd ax n = unsafePerformIO (withExPtr ax baz) where
+    baz :: Ptr GinacEx -> IO [Maybe Double]
+    baz ptr = do
+        x <- ginac_symbol_static
+        p <- ginac_ex_new_from_basic x
+        z <- ginac_ex_new_from_int 0
+        r <- ginac_relation_eq_new p z
+        s <- ginac_ex_series ptr r 500
+        -- ginac_basic_free r
+        -- a <- ginac_ex_coeff_symbol n s x
+        --ginac_ex_free s
+        --ginac_ex_free z
+        -- ginac_ex_free p
+        sequence [ginac_ex_coeff_symbol j s x >>= numcast | j <- [0 .. n]]
+    --baz :: Ptr GinacEx -> IO [Maybe Double]
+    --baz ptr = sequence [fn ptr i | i <- [0 .. n]]
+    --fn :: Ptr GinacEx -> Int -> IO (Maybe Double)
+    --fn ptr i = do
+    --    sy <- ginac_symbol_static 
+    --    y <- ginac_ex_coeff_symbol i ptr sy
+    --    numcast y
+
+showGF :: OGF -> IO ()
+showGF ax = withExPtr ax ginac_ex_print
 
 numcast :: Ptr GinacEx -> IO (Maybe Double)
 numcast ptr = do
@@ -82,23 +260,25 @@ numcast ptr = do
         then fmap Just (ginac_ex_to_double ptr)
         else pure Nothing
 
+--
+--
+--
+
 substitute :: OGF -> Int -> Maybe Double
-substitute ax x = unsafePerformIO (withOGF ax fn) where
+substitute ax x = unsafePerformIO (withExPtr ax fn) where
     fn ptr = ginac_symbol_static >>= ginac_ex_subs_int x ptr >>= numcast
 
-expanded :: OGF -> [Integer]
-expanded ax = maybe 0 round <$> series ax where
-    series :: OGF -> [Maybe Double]
-    series = rec 0 where
-        rec n ax = x:xs where
-            x  = substitute (ax / factorial n) 0
-            xs = rec (succ n) (ddx ax)
+series :: OGF -> [Maybe Double]
+series = rec 0 where
+    rec n ax = x:xs where
+        x  = substitute (ax / factorial n) 0
+        xs = rec (succ n) (ddx ax)
 
-expandedTo :: Int -> OGF -> [Integer]
-expandedTo n = take n . expanded
+--expand :: Int -> OGF -> Maybe [Integer]
+--expand n = (fmap . fmap) truncate . sequence . take n . series
 
-lsh :: Int -> OGF -> OGF
-lsh n ax = X^^negate n * ax
+expand n = take n . series
 
-rsh :: Int -> OGF -> OGF
-rsh n ax = X^^n * ax
+--nth :: Int -> OGF -> Maybe Integer
+--nth n = fmap last . expand (succ n)
+nth n ax = Just 0
