f30f7b2e68a2c167a83b7f2c7ab950ef3e85650e
Johannes Hilden <hildenjohannes@gmail.com>
2018-05-30 22:24:17 +0300

    Use convolution from functional pearl

--- src/Powerpants/Polynomial.hs
+++ src/Powerpants/Polynomial.hs
@@ -9,12 +9,11 @@ import Control.Applicative      ( pure )
 import NumericPrelude
 import NumericPrelude.Base
 
-newtype Polynomial a = Px { coefficients :: [a] } deriving (Show, Eq)
+newtype Polynomial a = Px { coefficients :: [a] }
+  deriving (Show, Eq)
 
-trim :: (Algebra.IntegralDomain.C a, Eq a)
-     => Polynomial a
-     -> Polynomial a
-trim = Px . canon . reverse . dropWhile (== 0). reverse . coefficients where
+trim :: (Algebra.IntegralDomain.C a, Eq a) => [a] -> [a]
+trim = canon . reverse . dropWhile (== 0) . reverse where
     canon [] = [zero]
     canon xs = xs
 
@@ -28,14 +27,9 @@ add :: (Algebra.IntegralDomain.C a, Eq a)
     => Polynomial a
     -> Polynomial a
     -> Polynomial a
-add (Px xs) (Px ys) = trim (Px (rec xs ys)) where
-    rec xs [] = xs
-    rec [] ys = ys
-    rec (x:xs) (y:ys) = x + y : rec xs ys
+add (Px xs) (Px ys) = Px (trim (xs + ys))
 
-neg :: (Algebra.IntegralDomain.C a, Eq a)
-    => Polynomial a
-    -> Polynomial a
+neg :: (Algebra.IntegralDomain.C a, Eq a) => Polynomial a -> Polynomial a
 neg (Px xs) = Px (fmap negate xs)
 
 instance (Algebra.IntegralDomain.C a, Eq a) => Algebra.Ring.C (Polynomial a)
@@ -47,9 +41,15 @@ mul :: (Algebra.IntegralDomain.C a, Eq a)
     => Polynomial a
     -> Polynomial a
     -> Polynomial a
-mul (Px xs) (Px ys) = trim (foldr1 add (rec xs ys)) where
-    rec [] _      = [0]
-    rec (x:xs) ys = Px (fmap (*x) ys) : rec xs (0 : ys)
+mul (Px xs) (Px ys) = Px (trim (rec xs ys)) where
+    rec [] ys = [0]
+    rec xs [] = [0]
+    rec (x:xs) (y:ys) = x * y : x.* ys + y.* xs + (0 : rec xs ys)
+    (.*) x = map (x*)
+
+--mul (Px xs) (Px ys) = trim (foldr1 add (rec xs ys)) where
+--    rec [] _      = [0]
+--    rec (x:xs) ys = Px (fmap (*x) ys) : rec xs (0 : ys)
 
 constant :: (Algebra.IntegralDomain.C a, Eq a) => a -> Polynomial a
 constant = Px . pure
@@ -60,7 +60,7 @@ x = Px [0, 1]
 derivative :: (Algebra.IntegralDomain.C a, Eq a, Enum a)
            => Polynomial a
            -> Polynomial a
-derivative (Px (_:xs)) = trim (Px (zipWith (*) xs [1..]))
+derivative (Px (_:xs)) = Px (trim (zipWith (*) xs [1..]))
 
 eval :: (Algebra.Ring.C a) => Polynomial a -> a -> a
 eval (Px xs) c = sum (zipWith (*) xs [c^n | n <- [0..]])
