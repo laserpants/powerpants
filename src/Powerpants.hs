aa69369855b79ed8e2d814e0d5ddf40107450266
Johannes Hilden <hildenjohannes@gmail.com>
2018-05-31 18:23:18 +0300

    Try new approach

--- src/Powerpants.hs
+++ src/Powerpants.hs
@@ -1,3 +1,3 @@
 module Powerpants where
 
-import Powerpants.Polynomial
+import Powerpants.Expr
--- src/Powerpants/Expr.hs
+++ src/Powerpants/Expr.hs
@@ -3,29 +3,51 @@
 module Powerpants.Expr where
 
 import Algebra.Additive
+import Algebra.Field
 import Algebra.IntegralDomain
 import Algebra.Ring
+import Algebra.ZeroTestable
+import MathObj.Polynomial
+import MathObj.Polynomial.Core   ( differentiate )
+import Number.Ratio
 import NumericPrelude
 import NumericPrelude.Base
-import Powerpants.Polynomial
-
-type P = Polynomial
 
 data Expr a
-  = Poly !(P a)        -- p(x)
-  | Frac !(P a) !(P a) -- p(x)/q(x)
-  | Recp !(P a)        -- 1/p(x)
+  = Px   !(MathObj.Polynomial.T a)
+  | Frac !(MathObj.Polynomial.T a)
+         !(MathObj.Polynomial.T a)
+--  | Pow  !(MathObj.Polynomial.T a) 
+--         !Integer
   deriving (Show, Eq)
 
-frac :: (Algebra.IntegralDomain.C a, Eq a, Enum a)
-     => Polynomial a
-     -> Polynomial a
+frac :: (Algebra.Field.C a, Algebra.ZeroTestable.C a, Eq a)
+     => MathObj.Polynomial.T a
+     -> MathObj.Polynomial.T a
      -> Expr a
-frac gx hx
-  |  1 == gx  = Recp hx
-  | -1 == gx  = Recp (negate hx)
-  | otherwise = Frac gx hx
+frac 0 q = Px 0
+frac p 1 = Px p
+frac p q = Frac a b where a :% b = p % q
 
-ddx :: (Algebra.IntegralDomain.C a, Eq a, Enum a) => Expr a -> Expr a
-ddx (Poly px) = Poly (derivative px)
-ddx (Recp gx) = frac (negate (derivative gx)) (gx^2)
+ddx :: (Algebra.Field.C a, Algebra.ZeroTestable.C a, Eq a)
+    => Expr a
+    -> Expr a
+ddx (Px px) = Px (diff px)
+ddx (Frac px qx) =
+    case coeffs px of
+      [n] -> frac (MathObj.Polynomial.const n * negate (diff qx)) (qx^2)
+      _   -> frac (diff px * qx - diff qx * px) (qx^2)
+-- ddx (Pow px n) = undefined
+
+diff :: (Algebra.Field.C a, Algebra.ZeroTestable.C a, Eq a)
+     => MathObj.Polynomial.T a
+     -> MathObj.Polynomial.T a
+diff = fromCoeffs . differentiate . coeffs
+
+eval :: (Algebra.Field.C a, Algebra.ZeroTestable.C a, Eq a) => Expr a -> a -> a
+eval (Px px)      x = evaluate px x
+eval (Frac px qx) x = evaluate px x / evaluate qx x
+-- eval (Pow px n)   x = (evaluate px x)^n
+
+xxx :: (Algebra.Field.C a, Algebra.ZeroTestable.C a, Eq a) => Expr a -> [a]
+xxx = rec where rec ex = eval ex 0 : rec (ddx ex)
