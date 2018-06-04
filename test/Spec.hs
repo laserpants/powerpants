ee8ccd3c499d54dca1a80106a069e3de627a49ae
Johannes Hilden <hildenjohannes@gmail.com>
2018-06-04 23:23:52 +0300

    Implement first tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,14 @@
+import Powerpants.Expr
+import Powerpants.Expr.Simplify
+import Test.Hspec
+
+testDivnode :: SpecWith ()
+testDivnode =
+
+    describe "divnode" $
+      it "eliminates intermediate division nodes" $ do
+        divnode (Div (Div (Num 1) (Num 2)) (Num 3)) `shouldBe` Div (Num 1) (Mul [Num 2, Num 3])
+        divnode (Div (Num 1) (Div (Num 2) (Num 3))) `shouldBe` Div (Mul [Num 1, Num 3]) (Num 2)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec testDivnode
