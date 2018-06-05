f53439c12523fe7d33fce9a86b4a6c7470b9c201
Johannes Hilden <hildenjohannes@gmail.com>
2018-06-05 16:50:33 +0300

    Update tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,21 +2,38 @@ import Powerpants.Expr
 import Powerpants.Expr.Simplify
 import Test.Hspec
 
+testFlattened :: SpecWith ()
+testFlattened =
+
+    describe "flattened" $
+      it "" $
+
+        ordered (flattened (Mul [Num 5, X, Mul [Num 3, Num 2], Num 2, X]))
+          `shouldBe` Mul [X,X,Num 2,Num 2,Num 3,Num 5]
+
 testCollectNums :: SpecWith ()
 testCollectNums =
 
     describe "collectNums" $
       it "" $ do
-        collectNums [Num 4, X] `shouldBe` ([4], [X])
-        collectNums [Num 5, X, Mul [Num 3, Num 2], Num 2, X] `shouldBe` ([5, 2], [X, Mul [Num 3, Num 2], X])
+
+        collectNums [Num 4, X]
+          `shouldBe` ([4], [X])
+
+        collectNums [Num 5, X, Mul [Num 3, Num 2], Num 2, X]
+          `shouldBe` ([5, 2], [X, Mul [Num 3, Num 2], X])
 
 testDivnode :: SpecWith ()
 testDivnode =
 
     describe "divnode" $
       it "eliminates intermediate division nodes" $ do
-        divnode (Div (Div (Num 1) (Num 2)) (Num 3)) `shouldBe` Div (Num 1) (Mul [Num 2, Num 3])
-        divnode (Div (Num 1) (Div (Num 2) (Num 3))) `shouldBe` Div (Mul [Num 1, Num 3]) (Num 2)
+
+        divnode (Div (Div (Num 1) (Num 2)) (Num 3))
+          `shouldBe` Div (Num 1) (Mul [Num 2, Num 3])
+
+        divnode (Div (Num 1) (Div (Num 2) (Num 3)))
+          `shouldBe` Div (Mul [Num 1, Num 3]) (Num 2)
 
 main :: IO ()
 main = hspec testDivnode
