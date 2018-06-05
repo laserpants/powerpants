1c8cca179fc456c362b0d8eab25d4f4ec1826e27
Johannes Hilden <hildenjohannes@gmail.com>
2018-06-05 16:40:35 +0300

    Add more tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,6 +2,14 @@ import Powerpants.Expr
 import Powerpants.Expr.Simplify
 import Test.Hspec
 
+testCollectNums :: SpecWith ()
+testCollectNums =
+
+    describe "collectNums" $
+      it "" $ do
+        collectNums [Num 4, X] `shouldBe` ([4], [X])
+        collectNums [Num 5, X, Mul [Num 3, Num 2], Num 2, X] `shouldBe` ([5, 2], [X, Mul [Num 3, Num 2], X])
+
 testDivnode :: SpecWith ()
 testDivnode =
 
