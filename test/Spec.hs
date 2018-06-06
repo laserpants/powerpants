533ada55da2f71126c880c2da61b35e6cc12f3e9
Johannes Hilden <hildenjohannes@gmail.com>
2018-06-06 16:21:35 +0300

    Comment out old tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,49 +2,40 @@ import Powerpants.Expr
 import Powerpants.Expr.Simplify
 import Test.Hspec
 
-testFlattened :: SpecWith ()
-testFlattened =
-
-    describe "flattened" $
-      it "" $
-
-        ordered (flattened (Mul [Num 5, X, Mul [Num 3, Num 2], Num 2, X]))
-          `shouldBe` Mul [X,X,Num 2,Num 2,Num 3,Num 5]
-
-testFolded :: SpecWith ()
-testFolded =
-
-      it "" $ do
-
-        eval (4 :: Double) (folded (Add [Num 5, X, Mul [Num 3, Num 2], Num 2, X]))
-          `shouldBe` eval 4 (Add [Num 5, X, Mul [Num 3, Num 2], Num 2, X])
-
-        eval (7 :: Double) (folded (Add [Num 5, X, Mul [Num 3, Num 2], Num 2, X]))
-          `shouldBe` eval 7 (Add [Num 5, X, Mul [Num 3, Num 2], Num 2, X])
-
-testCollectNums :: SpecWith ()
-testCollectNums =
-
-    describe "collectConsts" $
-      it "" $ do
-
-        collectConsts [Num 4, X]
-          `shouldBe` ([4], [X])
-
-        collectConsts [Num 5, X, Mul [Num 3, Num 2], Num 2, X]
-          `shouldBe` ([5, 2], [X, Mul [Num 3, Num 2], X])
-
-testDivnode :: SpecWith ()
-testDivnode =
-
-    describe "divnode" $
-      it "eliminates intermediate division nodes" $ do
-
-        divnode (Div (Div (Num 1) (Num 2)) (Num 3))
-          `shouldBe` Div (Num 1) (Mul [Num 2, Num 3])
-
-        divnode (Div (Num 1) (Div (Num 2) (Num 3)))
-          `shouldBe` Div (Mul [Num 1, Num 3]) (Num 2)
+--testFlattened :: SpecWith ()
+--testFlattened =
+--
+--    describe "flattened" $
+--      it "" $
+--
+--        ordered (flattened (Mul [Num 5, X, Mul [Num 3, Num 2], Num 2, X]))
+--          `shouldBe` Mul [X,X,Num 2,Num 2,Num 3,Num 5]
+--
+--testFolded :: SpecWith ()
+--testFolded =
+--
+--      it "" $ do
+--
+--        eval (4 :: Double) (folded (Add [Num 5, X, Mul [Num 3, Num 2], Num 2, X]))
+--          `shouldBe` eval 4 (Add [Num 5, X, Mul [Num 3, Num 2], Num 2, X])
+--
+--        eval (7 :: Double) (folded (Add [Num 5, X, Mul [Num 3, Num 2], Num 2, X]))
+--          `shouldBe` eval 7 (Add [Num 5, X, Mul [Num 3, Num 2], Num 2, X])
+--
+--testCollectNums :: SpecWith ()
+--testCollectNums =
+--
+--    describe "collectConsts" $
+--      it "" $ do
+--
+--        collectConsts [Num 4, X]
+--          `shouldBe` ([4], [X])
+--
+--        collectConsts [Num 5, X, Mul [Num 3, Num 2], Num 2, X]
+--          `shouldBe` ([5, 2], [X, Mul [Num 3, Num 2], X])
 
 main :: IO ()
-main = hspec testDivnode
+main = pure () -- hspec $ do
+--    testFlattened
+--    testFolded
+--    testCollectNums
