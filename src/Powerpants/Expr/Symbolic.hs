5675762d2f91370ddf59fb489d42d4c6ccf84c48
Johannes Hilden <hildenjohannes@gmail.com>
2018-06-07 21:33:22 +0300

    Implement Additive and Ring instances

---
+++ src/Powerpants/Expr/Symbolic.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RebindableSyntax #-}
+module Powerpants.Expr.Symbolic where
+
+import Algebra.Additive
+import Algebra.Ring
+import NumericPrelude
+import Powerpants.Expr
+
+instance Algebra.Ring.C a => Algebra.Additive.C (Expr a) where
+    a + b       = Add [a, b]
+    zero        = Num 0
+    negate      = neg
+
+instance Algebra.Ring.C a => Algebra.Ring.C (Expr a) where
+    a * b       = Mul [a, b]
+    fromInteger = Num . fromInteger
